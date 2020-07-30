27ed6f312200b6e776d5bd7f0634a600ef74c69a
tpim <tpim0901@gmail.com>
2020-07-30 20:28:27 +0800

    add currying demo explain md file .

---
+++ knowledge/currying.md
@@ -0,0 +1,61 @@
+# 柯里化
+
+## 1.概念
+
+> 柯里化（英语：Currying），又译为卡瑞化或加里化，是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
+
+> 在直觉上，柯里化声称“如果你固定某些参数，你将得到接受余下参数的一个函数”。
+> 所以对于有两个变量的函数，如果固定了{\displaystyle y=2}y=2，则得到有一个变量的函数{\displaystyle 2^{x}}2^{x}。
+> 在理论计算机科学中，柯里化提供了在简单的理论模型中，比如：只接受一个单一参数的 lambda 演算中，研究带有多个参数的函数的方式。
+
+> 函数柯里化的对偶是 Uncurrying，一种使用匿名单参数函数来实现多参数函数的方法。例如：
+
+```javascript
+var foo = function (a) {
+  return function (b) {
+    return a * a + b * b;
+  };
+};
+```
+
+## 2.作用
+
+柯里化有 3 个常见作用：
+
+- 参数复用；
+- 提前返回；
+- 延迟计算/运行。
+
+## 3.例子
+
+```javascript
+let setTail = function (tail) {
+  return function (content) {
+    return content + tail;
+  };
+};
+
+let newArr = [1, 2, 3, 4].map(setTail("~"));
+console.log(newArr);
+```
+
+```javascript
+let surprise = setSurprise(
+  function () {
+    let allOfContent = [].slice.call(arguments);
+    console.log(allOfContent);
+  },
+  "100000",
+  "10000000"
+);
+
+function setSurprise(fn) {
+  let args = [].slice.call(arguments, 1);
+  return function () {
+    let newArgss = args.concat([].slice.call(arguments));
+    return fn.apply(null, newArgss);
+  };
+}
+
+surprise(1, 2, 3, 4);
+```
--- test.js
+++ test.js
@@ -1,19 +1,68 @@
-function Person(first, last, age, gender, interests) {
-  // property and method definitions
-  this.name = {
-    "first": first,
-    "last": last,
+// function Person(first, last, age, gender, interests) {
+//   // property and method definitions
+//   this.name = {
+//     "first": first,
+//     "last": last,
+//   };
+//   this.age = age;
+//   this.gender = gender;
+//   //...see link in summary above for full definition
+// }
+
+// let person1 = new Person("Bob", "Smith", 32, "male", ["music", "skiing"]);
+// console.log(person1);
+
+// Person.prototype.say = function () {
+//   console.log(" hihi hi ");
+// };
+
+// person1.say();
+
+// let concatArray = function (chars) {
+//   return chars.reduce(function (a, b) {
+//     return a.concat(b);
+//   });
+// };
+
+// let concatArray = function (chars) {
+//   return chars
+//     .map(function (e) {
+//       return +e + 1 + "";
+//     })
+//     .reduce(function (a, b) {
+//       return a.concat(b);
+//     });
+// };
+
+// let res = concatArray(["1", "2", "3"]);
+// console.log(res);
+
+//柯里化
+let mutiple = function (a) {
+  return function (b) {
+    return +b * a + "";
   };
-  this.age = age;
-  this.gender = gender;
-  //...see link in summary above for full definition
-}
-
-let person1 = new Person("Bob", "Smith", 32, "male", ["music", "skiing"]);
-console.log(person1);
-
-Person.prototype.say = function () {
-  console.log(" hihi hi ");
 };
 
-person1.say();
+let plus = function (a) {
+  return function (b) {
+    return +b + a + "";
+  };
+};
+
+var concatArray = function (chars, stylishChar) {
+  return chars.map(stylishChar).reduce(function (a, b) {
+    return a.concat(b);
+  });
+};
+
+console.log(concatArray(["1", "2", "3"], plus(2)));
+
+let setTail = function (tail) {
+  return function (content) {
+    return content + tail;
+  };
+};
+
+let newArr = [1, 2, 3, 4].map(setTail("~"));
+console.log(newArr);
